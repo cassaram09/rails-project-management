f5ad77ef7311b237c7c5386b79c8b2468e42ede9
cassaram09 <cassaram09@gmail.com>
2016-07-20 18:16:42 -0400

    changed profile page so edit functions are located on /profile, form hidden with js, header title changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,19 @@
 
 $(document).ready(function(){
   $( "#show" ).click(function() {
-    $( ".quick-add" ).slideToggle( "slow", "linear" );
+    $( ".quick-add" ).slideToggle( "fast", "linear" );
     if ($( "#show" ).val() == "Add a Task"){
       $( "#show" ).val("Hide");
     } else {
       $( "#show" ).val("Add a Task");
+    }
+  });
+  $( "#show-profile-options" ).click(function() {
+    $( ".profile-options" ).slideToggle( "slow", "swing" );
+    if ($( "#show-profile-options" ).val() == "Edit Profile"){
+      $( "#show-profile-options" ).val("Hide Options");
+    } else {
+      $( "#show-profile-options" ).val("Edit Profile");
     }
   });
 });

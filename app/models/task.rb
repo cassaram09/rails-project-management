5034cd0fcd66a9c96a85ad3fa36f17d52ea0ab0a
cassaram09 <cassaram09@gmail.com>
2016-07-22 18:08:39 -0400

    updated overdue scope for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,7 +16,7 @@ class Task < ActiveRecord::Base
 
   scope :complete, -> { where(status: 1) }
   scope :active, -> { where(status: 0)}
-  scope :overdue, -> { where("due_date < ?", Date.today)}
+  scope :overdue, -> { where("due_date < ? AND status = ?", Date.today, 0)}
   scope :search, -> (search, user) { where("(name LIKE ? OR description LIKE ?) AND user_id = ?", "%#{search}%", "%#{search}%", user.id)}
 
   #Custom writer and readers for Task for creating nested attributes

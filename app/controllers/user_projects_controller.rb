78355d554a10317001a48d0356c18734b3c5e3ff
cassaram09 <cassaram09@gmail.com>
2016-07-23 18:34:50 -0400

    add strong params to UserProject controller

--- app/controllers/user_projects_controller.rb
+++ app/controllers/user_projects_controller.rb
@@ -1,10 +1,13 @@
 class UserProjectsController < ApplicationController
   def edit_user_project_permission
-    up = UserProject.find_by(id: params[:user_project][:id])
-    binding.pry
-    up.permission = params[:user_project][:permission]
-    binding.pry
+    up = UserProject.find_by(id: user_project_params[:id])
+    up.permission = user_project_params[:permission]
     up.save
-    redirect_to edit_project_path(params[:user_project][:project_id])
+    redirect_to edit_project_path(user_project_params[:project_id])
+  end
+
+  private
+  def user_project_params
+    params.require(:user_project).permit(:id, :permission, :project_id)
   end
 end

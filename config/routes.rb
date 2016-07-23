5597a12d0b19e9f6bd3ba1c0412c8c362b871f0f
cassaram09 <cassaram09@gmail.com>
2016-07-22 22:03:33 -0400

    remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
-  get 'search', to: 'home#search', as: 'search'
-
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations' }, skip: [:sessions] 
     as :user do
       get 'login' => 'devise/sessions#new', :as => :new_user_session
@@ -18,26 +16,18 @@ Rails.application.routes.draw do
 
   resources :tags
 
-  # resources :responsibilities
-
-  # post "/tasks/new", to: 'tasks#create', as: "post_new_task"
-  # get "/tasks/new", to: 'tasks#create', as: "create_new_task"
-  # get "/tasks/complete", to: 'tasks#complete', as: "complete_tasks"
-  # get "/tasks/all", to: 'tasks#all', as: 'all_tasks'
-
   scope "/tasks" do
     get'/new', to: 'tasks#quick_new_task', as: "quick_new_task"
     post'/', to: 'tasks#create', as: "post_quick_new_task"
     get'/all', to: 'tasks#all_tasks', as: "all_tasks"
   end
 
-  # post "/projects/new", to: "projects#create", as: "post_new_project"
-
   resources :projects do
     get :overdue, on: :collection
     get :complete, on: :collection
 
     resources :comments, :notes, shallow: true
+
     resources :tasks, shallow: true do 
       get :complete, on: :collection, to: "tasks#complete"
       get :overdue, on: :collection, to: "tasks#overdue"

94b51998591b2fee87915bc38598d55c9fa8462a
cassaram09 <cassaram09@gmail.com>
2016-07-24 14:59:52 -0400

    update UserProject enum capitalization, update edit_collaborators style, replace select with clean f.select on edit_collaborators form

--- app/models/user_project.rb
+++ app/models/user_project.rb
@@ -2,7 +2,7 @@ class UserProject < ActiveRecord::Base
   belongs_to :collaboration_project, class_name: "Project"
   belongs_to :collaborator, class_name: "User"
 
-  enum permission: [:view, :edit]
+  enum permission: [:View, :Edit]
 
   scope :projects, ->  (project_id) { where("collaboration_project_id = ?", project_id )}
 end

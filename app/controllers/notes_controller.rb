2b80e435bfbf62eea32271a69c6e31e957224349
cassaram09 <cassaram09@gmail.com>
2016-07-24 15:57:59 -0400

    clean up code in notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,6 +3,8 @@ class NotesController < ApplicationController
   before_action :set_project, except: [:show, :edit]
   before_action :set_note, except: [:index, :new, :create]
 
+   ## STANDARD RESTFUL ROUTES
+
   def index
     @notes = @project.notes.reverse
   end
@@ -38,16 +40,14 @@ class NotesController < ApplicationController
     redirect_to project_notes_path(@project)
   end
 
+  ## PRIVATE METHODS
+
   private
   def set_note
     @note = Note.find_by(id: params[:id])
   end
 
-  def note_params
-    params.require(:note).permit(:title, :content, :user_id, :project_id)
-  end
-
-  def set_project
+   def set_project
     if params[:id]
        @project = Project.find_by(id: params[:id])
     else
@@ -55,4 +55,7 @@ class NotesController < ApplicationController
     end
   end
 
+  def note_params
+    params.require(:note).permit(:title, :content, :user_id, :project_id)
+  end
 end
